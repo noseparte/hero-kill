1a3c4b7b338e089910968bf848359aaafea06c14
noseparte <noseparte@aliyun.com>
2020-01-02 14:47:39 +0800

    Initial commit

---
+++ src/main/java/com/hero/herokill/HeroKillApplication.java
@@ -0,0 +1,13 @@
+package com.hero.herokill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeroKillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hero/herokill/HeroKillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hero.herokill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroKillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

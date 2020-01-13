713a777c9a3a643d1a98619ab2bc88e54af5ff2b
noseparte <noseparte@aliyun.com>
2020-01-13 18:31:48 +0800

    new commercial

---
+++ game/src/main/java/com/hk/game/GameConfiguration.java
@@ -0,0 +1,21 @@
+package com.hk.game;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Auther: Noseparte
+ * @Date: 2020/1/8 15:14
+ * @Description:
+ *
+ *         <p></p>
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "game")
+public class GameConfiguration {
+
+    public int poolSize;
+
+}
---
+++ game/src/main/java/com/hk/game/HeroKillGameApplication.java
@@ -0,0 +1,21 @@
+package com.hk.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: Noseparte
+ * @Date: 2020/1/8 15:01
+ * @Description:
+ *
+ *         <p>Game服 </p>
+ *         <p>游戏业务逻辑 </p>
+ */
+@SpringBootApplication
+public class HeroKillGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeroKillGameApplication.class);
+    }
+
+}
